9076cc4b0bdfec2fd275a02dede617fe670b5598
Aslanch1k <zxerfgty@gmail.com>
2024-01-06 15:08:05 +0200

    Add delete category

--- flaskproj/views.py
+++ flaskproj/views.py
@@ -1,13 +1,17 @@
-from flask import  jsonify, request
+from flask import jsonify, request
 from flaskproj import app
+import uuid
+from datetime import datetime
 
 
 CATEGORIES = [
     {
         "id": 1,
-        "name": "Food"
+        "name": "food"
     }
 ]
+
+USERS = []
 
 
 # GET /categories
@@ -21,6 +25,23 @@ def get_categories():
 @app.route("/category", methods=['POST'])
 def create_category():
     request_data = request.get_json()
-    CATEGORIES.append(request_data)
+    category_id = str(uuid.uuid4())
+    CATEGORIES.append({"id": category_id, "name": request_data["name"]})
     return jsonify(request_data)
+
+
+@app.route("/category", methods=["DELETE"])
+def delete_category():
+    request_data = request.get_json()
+    for i in CATEGORIES:
+        if i['id'] == request_data["id"]:
+            deleted_category = i
+            break
+    CATEGORIES.remove(deleted_category)
+    return jsonify(deleted_category)
+
+
+@app.route("/categories")
+def get_categories():
+    return jsonify({"categories": CATEGORIES})
 
